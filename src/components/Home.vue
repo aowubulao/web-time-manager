9c4f5aa60b7ba71fbe05531da3b068a3c043062d
Neo_Zzj <silenceneo@qq.com>
2019-12-06 17:00:35 +0800

    timaner

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -89,15 +89,4 @@
         letter-spacing: 1px;
         font-weight: 600;
     }
-    .bottom-layout {
-        width: 100%;
-        height: 20px;
-        background-color: #409EFF;
-    }
-    .navBarWrap {
-        position:fixed;
-        top:0;
-        z-index:999;
-        width: 100%;
-    }
 </style>
